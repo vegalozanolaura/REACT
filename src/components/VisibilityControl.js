1c91d8263b68e3e1e4ab611538fcf36059c624e1
vegalozanolaura <106939179+vegalozanolaura@users.noreply.github.com>
2022-09-23 20:45:50 -0500

    Add files via upload

---
+++ src/components/VisibilityControl.js
@@ -0,0 +1,26 @@
+export const VisibilityControl = ({isChecked, setshowCompleted, cleanTasks}) => {
+  
+    const handleDelete = () => {
+        if (window.confirm('Are you sure you want to delete this task?')){
+        cleanTasks()
+     }
+    }
+  
+    return (
+    <div className="d-flex justify-content-between bg-secondary text-white text-center p-2 border-secondary">
+     <div className="form-check form-switch">
+      <input
+          className="form-check-input"
+          type="checkbox"
+          checked={isChecked}
+          onChange={(e) => setshowCompleted(e.target.checked)}
+        />{" "}
+        <label>Show Tasks Done</label>
+     </div>
+
+      <button onClick={handleDelete} className="btn btn-danger btn-sm">
+        Clear
+      </button>
+    </div>
+  );
+};
